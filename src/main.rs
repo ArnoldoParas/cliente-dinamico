fc5abfd5af9925622f405fa5dd7aa7f45f687c5c
Arnoldo Arturo <arnoldoarturoxd@gmail.com>
2024-05-06 18:54:23 -0600

    added web socket conection

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,59 @@
-use sysinfo::System;
-use std::net::{TcpStream, Shutdown};
-use std::io::prelude::*;
+use sysinfo::{ System, Networks, Disks};
+// use std::net::{TcpStream, Shutdown};
+// use std::io::prelude::*;
+
+use tungstenite::connect;
+use std::thread;
+use std::time::Duration;
 
 
 fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:5432").unwrap();
-
-
-
-
     let mut sys = System::new_all();
+    let mut bandwith: u64 = 0;
+    let mut freebandwith: u64 = 0;
     sys.refresh_all();
+    
+    let mut networks = Networks::new_with_refreshed_list();
+    for (interface_name, network) in &networks {
+        bandwith = network.total_transmitted() + network.total_received();
+        // println!("out: {}MB",network.total_transmitted()/1_000_000);
+        // println!("in: {}MB",network.total_received()/1_000_000);
+    }
 
-    let cpu = sys.cpus().get(0).unwrap();
+    let disks = Disks::new_with_refreshed_list();
+    for disk in disks.list() {
+        println!("{:?}", disk.available_space()/1_000_000_000);
+        break;
+    }
+    
+    let (mut ws,_) = connect("ws://127.0.0.1:5432").unwrap();
+    loop {
+        
+        networks.refresh();
+        for (interface_name, network) in &networks {
+            // freebandwith = network.transmitted() + network.received();
+            freebandwith = bandwith - (network.transmitted() + network.received());
+        }
+        let cpu = sys.cpus().get(0).unwrap();
+        
+        let sysinfo = format!("{:.2}%,{} mb, {} Mb",
+        cpu.cpu_usage(),
+        sys.used_memory()/1_000_000,
+        freebandwith/1_000_000
+        );
+        sys.refresh_all();
 
-    // println!("Modelo del procesador: {}", cpu.brand());
-    // println!("Frecuencia del procesador: {}", cpu.frequency());
-    // println!("Numero de procesadores logicos: {}", sys.cpus().len());
-    // println!("Numero de nucleos fisicos: {}", sys.physical_core_count().unwrap());
-    // println!("Memoria total: {} bytes", sys.total_memory());
-    // println!("Version del sistema operativo: {}, version: {}", 
-    //     System::long_os_version().unwrap(),
-    //     System::kernel_version().unwrap());
-    let sysinfo = format!("{},{},{},{},{},{},{}",
-        System::host_name().unwrap(),
-        cpu.brand().trim(),
-        cpu.frequency(),
-        sys.physical_core_count().unwrap(),
-        sys.total_memory(),
-        System::long_os_version().unwrap(),
-        System::kernel_version().unwrap()
-    );
-    println!("{}", sysinfo);
-    stream.write_all(sysinfo.as_bytes()).unwrap();
-    stream.shutdown(Shutdown::Both).expect("fail to shut down write");
+        thread::sleep(Duration::from_secs(1));
+        // let sysinfo = format!("{},{},{},{},{},{},{}",
+        // System::host_name().unwrap(),
+        // cpu.brand().trim(),
+        // cpu.frequency(),
+        // sys.physical_core_count().unwrap(),
+        // sys.total_memory(),
+        // System::long_os_version().unwrap(),
+        // System::kernel_version().unwrap()
+        // );
+        println!("{}", sysinfo);
+        ws.send(tungstenite::Message::Text(sysinfo));
+    }
 }
